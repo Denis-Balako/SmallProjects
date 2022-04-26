e66bc7b992d011f4b6426722167955bd0f0e07b9
ZVIIR <denisarius2000s@gmail.com>
2022-04-26 12:57:59 +0300

    Make some changes

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -14,4 +14,44 @@ capitals = {'Айдахо': 'Бойсе', 'Айова': 'Де-Мойн', 'Ала
             'Луизиана': 'Батон-Руж', 'Массачусетс': 'Бостон', 'Миннесота': 'Сент-Пол', 'Миссисипи': 'Джэксон',
             'Миссури': 'Джефферсон-Сити', 'Мичиган': 'Лансинг', 'Монтана': 'Хелена', 'Мэн': 'Огаста',
             'Мэриленд': 'Аннаполис', 'Небраска': 'Линкольн', 'Невада': 'Карсон-Сити', 'Нью-Джерси': 'Трентон',
-            }
+            'Нью-Йорк': 'Олбани', 'Нью-Мексико': 'Санта-Фе', 'Нью-Гемпшир': 'Конкорд', 'Огайо': 'Колумбус',
+            'Оклахома': 'Оклахома-Сити', 'Орегон': 'Сейлем', 'Пенсильвания': 'Гаррисберг', 'Род-Айленд': 'Провиденс',
+            'Северная Дакота': 'Бисмарк', 'Северная Каролина': 'Роли', 'Теннесси': 'Нашвилл', 'Техас': 'Остин',
+            'Флорида': 'Таллахасси', 'Южная Дакота': 'Пирр', 'Южная Каролина': 'Колумбия', 'Юта': 'Солт-Лейк-Сити'}
+
+#Генерирование 35 файлов билетов
+for quizNum in range(35):
+    #Создание файлов и ключей ответов
+    quizFile = open(f'capitalquiz{quizNum + 1}.txt','w')
+    answerKeyFile = open(f'capitalquiz_answers{quizNum + 1}.txt', 'w')
+
+    #Запись заголовка билета
+    quizFile.write('Имя:\n\nДата:\n\nГруппа:\n\n')
+    quizFile.write((' ' * 20) + f'Столицы штатов (билет {quizNum + 1})')
+    quizFile.write('\n\n')
+
+    #Перемешевание порядка следования штатов
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    # Организация цикла по всем 50 штатам
+    # и создание вопроса для каждого из них
+    for questionNum in range(50):
+        # Получение правильных и неправильных ответов
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+        # Запись вариантов вопросов и ответов в файл билета
+        quizFile.write(f'{questionNum + 1}. Выберете столицу штата {states[questionNum]}.\n\n')
+        for i in range(4):
+            quizFile.write(f"   {'АБВГ'[i]}. {answerOptions[i]}\n")
+            quizFile.write('\n')
+
+            # Запись ключа ответа в файл
+            answerKeyFile.write(f"{questionNum + 1}. {'АБВГ'[answerOptions.index(correctAnswer)]}\n")
+    quizFile.close()
+    answerKeyFile.close()
